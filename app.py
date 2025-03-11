29738a0a861165ca8866a4295c471f1cac01599e
Prayesh13 <prayeshgodhani04@gmail.com>
2025-03-11 18:02:40 +0530

    add about page and some minor changes to contact and digital footprint file

--- app.py
+++ app.py
@@ -68,5 +68,9 @@ def contact():
 def roadmaps():
     return render_template('roadmaps.html')
 
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
 if __name__ == '__main__':
     app.run(debug=True, port=5001)
